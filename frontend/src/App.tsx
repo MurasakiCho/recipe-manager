803e4791e8df2b01ee4f9a6f8c87cdfb97c89c49
Sylrelessa <briana.oneal823@gmail.com>
2025-10-28 07:40:25 -0400

    GET /recipes fetch from UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,32 @@
 import { Sidebar, RecipeList, AddRecipeForm } from "./components";
 import type { Category, Recipe } from "./types";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css"
 
 const categories: Category[] = [
   { id: 1, category: "Breakfast"},
   { id: 2, category: "Lunch"},
   { id: 3, category: "Dinner"},
-]
+];
 
-const starterRecipes: Recipe[] = [
-  {id: 1, categoryId: 1, recipeName: "Pancakes", ingredients: "Flour, Sugar, Eggs, Milk", instructions: "Mix and cook."},
-  {id: 2, categoryId: 2, recipeName: "Tacos", ingredients: "Beef, Tortillas, Cheese", instructions: "Assemble and serve."},
-  {id: 3, categoryId: 3, recipeName: "Spaghetti", ingredients: "Pasta, Tomato Sauce", instructions: "Boil pasta, add sauce and serve."}
-]
+// const starterRecipes: Recipe[] = [
+//   {id: 1, categoryId: 1, recipeName: "Pancakes", ingredients: "Flour, Sugar, Eggs, Milk", instructions: "Mix and cook."},
+//   {id: 2, categoryId: 2, recipeName: "Tacos", ingredients: "Beef, Tortillas, Cheese", instructions: "Assemble and serve."},
+//   {id: 3, categoryId: 3, recipeName: "Spaghetti", ingredients: "Pasta, Tomato Sauce", instructions: "Boil pasta, add sauce and serve."}
+// ]
+
+
 
 export default function App() {
   const [selectedCategoryId, setCategoryId] = useState<number | null>(null);
-  const [recipes, setRecipes] = useState<Recipe[]>(starterRecipes);
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
   const [recipeToEdit, setRecipeToEdit] = useState<Recipe | null>(null);
+
+  useEffect(() => {
+  fetch("http://localhost:8080/api/recipes") //returns a promise
+    .then(res => res.json()) //parses HTTP response into a JS object (parsed JSON)
+    .then(data => setRecipes(data)); 
+  });
 
   const handleAddRecipe = (newRecipe: Recipe) => {
     setRecipes([...recipes, newRecipe]);
@@ -26,11 +34,11 @@ export default function App() {
 
   const handleDeleteRecipe = (id: number) => {
     setRecipes(recipes.filter(r => r.id !== id));
-  }
+  };
 
   const handleEditRecipe = (editedRecipe: Recipe) => {
     setRecipes(recipes.map(r => r.id === editedRecipe.id ? editedRecipe : r));
-  }
+  };
 
   return (
     <div className="App">
